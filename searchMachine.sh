f7fc0f4fc9a1bc0277f426b2de4bbda2515562c8
CasasPIERO <76080568+CasasPIERO@users.noreply.github.com>
2024-02-18 04:48:37 +0000

    Se agregó función para el panel de ayuda y para filtar por nombre de máquina la cuál nos mostrará su información

--- searchMachine.sh
+++ searchMachine.sh
@@ -1,1 +1,43 @@
-echo "Hola Mundo"
+#!/bin/bash
+
+function ctrl_c(){
+  echo -e "[+] Saliendo...\n"
+  exit 1
+}
+
+#CTRL_C
+trap ctrl_c INT
+
+function helpPanel(){
+  echo -e "\nSe muestra el panel de ayuda:"
+  echo -e "\t[+] -h: Muestra el panel de ayuda."
+  echo -e "\t[+] -m: Muestra la información de la máquina indicada."
+  echo
+}
+
+function searchMachine(){
+  checkMachine="$(cat bundle-temporal.js | awk "/name: \"$machineName\"/,/resuelta:/" | grep -vE "sku:|id:|resuelta:" | tr -d '"' | tr -d ',' | sed 's/ //g')"
+  if [ "$checkMachine" ]; then
+    echo -e "\nMostrando los datos de la máquina $machineName:"
+    cat bundle-temporal.js | awk "/name: \"$machineName\"/,/resuelta:/" | grep -vE "sku:|id:|resuelta:" | tr -d '"' | tr -d ',' | sed 's/ //g'
+  else
+    echo -e "\nNo se encontró la máquina $machineName"
+  fi
+  echo
+}
+
+#Variables
+declare -i parameter=0
+
+while getopts "m:h" opt; do
+  case $opt in
+    m) machineName="$OPTARG"; parameter=1;;
+    h);;
+  esac
+done
+
+if [ $parameter -eq 0 ]; then
+  helpPanel
+elif [ $parameter -eq 1 ]; then
+  searchMachine $machineName
+fi
